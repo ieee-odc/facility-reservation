f8d7f4f17d4b71ffd5c9e1267f48a5a2b00ed524
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-07-05 14:09:48 +0100

    login UI implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { MdLockOutline } from "react-icons/md";
+import { IoMail } from "react-icons/io5";
+import "./styles.css";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <h2>Member login</h2>
+      <form className="form-container">
+        <div className="inputs-container">
+          <div className="input-container">
+            <div className="icon-box">
+              <IoMail className="input-icon" />
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                className="input-with-icon"
+              />
+            </div>
+          </div>
+          <div className="input-container">
+            <div className="icon-box">
+              <MdLockOutline className="input-icon" />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                className="input-with-icon"
+              />
+            </div>
+          </div>
+        <div className="forgot-password-link">
+          <a href="#">Forgot password?</a>
+        </div>
+          <button className="login-submit-button login-buttons" type="submit">
+            Sign in
+          </button>
+          <div className="or">
+          <hr />
+         <span>OR</span>
+        <hr />
+        </div>
+      <button className="social-login google login-buttons ">Continue with Google</button>
+      <button className="social-login microsoft login-buttons">
+        Continue with Microsoft
+      </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
