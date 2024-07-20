d5f10072f0c36f6dd7f3f97d69e15826fbb2a483
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-07-20 17:35:35 +0100

    added a note that appears when hovering on an editable item

--- frontend/src/components/EditableField.jsx
+++ frontend/src/components/EditableField.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faX } from "@fortawesome/free-solid-svg-icons";
 
@@ -12,35 +12,53 @@ const EditableField = ({
   handleFieldChange,
   handleCancel,
   handleApprove,
-}) => (
-  <div className="about-content-element">
-    <img src={iconSrc} alt={name} className="icon about-content-element-one" />
-    {editingField === name ? (
-      <div className="edited-input about-content-element-two">
-        <input
-          placeholder={placeholder}
-          name={name}
-          value={value}
-          onChange={handleFieldChange}
-          className="input"
+  tooltipText = "You can edit this property here",
+}) => {
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+  return (
+    <>
+      <div
+        className="about-content-element"
+        onMouseEnter={() => setTooltipVisible(true)}
+        onMouseLeave={() => setTooltipVisible(false)}
+      >
+        <img
+          src={iconSrc}
+          alt={name}
+          className="icon about-content-element-one"
         />
-        <div className="interaction-buttons">
-          <div className="buttons">
-            <button onClick={handleApprove}>
-              <FontAwesomeIcon icon={faCheck} />
-            </button>
-            <button onClick={handleCancel}>
-              <FontAwesomeIcon icon={faX} />
-            </button>
+        {editingField === name ? (
+          <div className="edited-input about-content-element-two">
+            <input
+              placeholder={placeholder}
+              name={name}
+              value={value}
+              onChange={handleFieldChange}
+              className="input"
+            />
+            <div className="interaction-buttons">
+              <div className="buttons">
+                <button onClick={handleApprove}>
+                  <FontAwesomeIcon icon={faCheck} />
+                </button>
+                <button onClick={handleCancel}>
+                  <FontAwesomeIcon icon={faX} />
+                </button>
+              </div>
+            </div>
           </div>
-        </div>
+        ) : (
+          <p
+            className="about-content-element-two"
+            onClick={() => handleEdit(name)}
+          >
+            {value || placeholder}
+          </p>
+        )}
+        {(isTooltipVisible && editingField!==name) && <div className="tooltip">{tooltipText}</div>}
       </div>
-    ) : (
-      <p className="about-content-element-two" onClick={() => handleEdit(name)}>
-        {value || placeholder}
-      </p>
-    )}
-  </div>
-);
+    </>
+  );
+};
 
 export default EditableField;
