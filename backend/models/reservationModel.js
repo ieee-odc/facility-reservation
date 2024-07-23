82515f0ef172803ceba5332688cd2bd5924aa7d4
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-07-23 11:17:00 +0100

    reservation model

---
+++ backend/models/reservationModel.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const reservationSchema = mongoose.Schema(
+  {
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    motive: {
+      type: String,
+      required: true,
+    },
+    materials: {
+      type: [String],
+      required: true,
+    },
+    files: {
+      type: [String],
+      required: true,
+    },
+    effective: {
+      type: Number,
+      required: true,
+    },
+    facility: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Facility",
+      required: true,
+    },
+    entity: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ReservationInitiator",
+      required: true,
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: false,
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Reservation = mongoose.model("reservations", reservationSchema);
