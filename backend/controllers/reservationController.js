4f8189464be69c3be597e0d513a854101cd4392e
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-07-23 13:12:20 +0100

    reservation controllers

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -11,7 +11,107 @@ import { Reservation } from "../models/reservationModel.js";
   event:
   state: 
 */
+
+export const findReservationById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const reservation = await Reservation.findById(id);
+
+    if (reservation) {
+      return res.status(200).json(reservation);
+    } else {
+      return res.status(404).json({ message: "Reservation not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const findAllReservations = async (req, res) => {
+  try {
+    const reservations = await Reservation.find();
+
+    if (reservations.length > 0) {
+      return res.status(200).json(reservations);
+    } else {
+      return res.status(404).json({ message: "No reservations found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+
 export const addReservation = async (req, res) => {
   try {
-  } catch (error) {}
+    const {
+      facility,
+      motive,
+      date,
+      time,
+      state,
+      entity,
+      event,
+      effective,
+      materials,
+      files,
+    } = req.body;
+    const reservation = await Reservation.create({
+      facility,
+      motive,
+      date,
+      time,
+      state,
+      entity,
+      event,
+      effective,
+      materials,
+      files,
+    });
+
+    if(reservation) return res.status(201).json(reservation);
+    return res.status(201).json({message: "an error occured when creating the reservation"});
+  } catch (error) {
+
+    return res.status(409).json({ message: error.message });
+  }
 };
+
+export const updateReservation = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateFields = req.body;
+
+    const reservation = await Reservation.findByIdAndUpdate(id, updateFields, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (reservation) {
+      return res.status(200).json(reservation);
+    } else {
+      return res.status(404).json({ message: "Reservation not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteReservation = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const reservation = await Reservation.findByIdAndDelete(id);
+
+    if (reservation) {
+      return res.status(200).json({ message: "Reservation deleted successfully" });
+    } else {
+      return res.status(404).json({ message: "Reservation not found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+
