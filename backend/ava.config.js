78d0922129daff6a37d586fe92154a8001bbb55e
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-07-30 00:48:09 +0100

    test

--- backend/ava.config.js
+++ backend/ava.config.js
@@ -1,9 +1,9 @@
 export default {
   files: [
-    /*"test/facility.test.js",
-    "test/equipment.test.js",*/
+    "test/facility.test.js",
+    "test/equipment.test.js",
     "test/event.test.js",
-    //"test/**/*.test.js",
+    "test/**/*.test.js",
     //'other-tests/**/*.test.js',
   ],
   serial: true
