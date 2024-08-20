c865691b02a64f5efba2718c280e4b06802a9108
Yosr Ghozzi <yosr.ghozzi@insat.ucar.tn>
2024-08-21 00:03:49 +0100

    new style

--- frontend/src/components/adminDashboard/AdminView.jsx
+++ frontend/src/components/adminDashboard/AdminView.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Panel } from "rsuite";
 import "./AdminView.css";
 import { getAllEvents, getAllPureReservations } from "../../apiService";
 import Navbar from "../navbar";
@@ -33,96 +34,107 @@ const AdminView = () => {
   };
 
   return (
-    <div className="admin-view">
+    <div>
       <Navbar />
-      <div className="content-container">
-        <div className="section events-section">
-          <h2>Events</h2>
-          {events.map((event) => (
-            <div key={event._id} className="event-panel">
-              <div className="event-header">
-                <h3>{event.name}</h3>
-                <p>{event.description}</p>
-              </div>
-              <div className="event-details">
-                <p>
-                  <strong>Start Date:</strong>{" "}
-                  {new Date(event.startDate).toLocaleDateString()}
-                </p>
-                <p>
-                  <strong>End Date:</strong>{" "}
-                  {new Date(event.endDate).toLocaleDateString()}
-                </p>
-                <p>
-                  <strong>State:</strong> {event.state}
-                </p>
-                <p>
-                  <strong>Organizer:</strong> {event.organizer}
-                </p>
-                <p>
-                  <strong>Total Effective:</strong> {event.totalEffective}
-                </p>
-                <p>
-                  <strong>Reservations:</strong>
-                </p>
+      <div className="admin-view">
+        <div className="content-container">
+          <div className="section events-section">
+            <h2>Events</h2>
+            {events.map((event) => (
+              <Panel
+                header={
+                  <div className="event-header">
+                    <h5 >{event.organizer}</h5>
+                    <h6>{event.name}</h6>
+                  </div>
+                }
+                className="event-panel"
+                collapsible
+                bordered
+              >
+                <div key={event._id}>
+                  <div className="event-details">
+                    <p>
+                      <strong>Description:</strong> {event.description}
+                      
+                    </p>
+                    <p>
+                      <strong>Start Date:</strong>{" "}
+                      {new Date(event.startDate).toLocaleDateString()}
+                    </p>
+                    <p>
+                      <strong>End Date:</strong>{" "}
+                      {new Date(event.endDate).toLocaleDateString()}
+                    </p>
+                    <p>
+                      <strong>State:</strong> {event.state}
+                    </p>
+                    <p>
+                      <strong>Total Effective:</strong> {event.totalEffective}
+                    </p>
+                    <p>
+                      <strong>Reservations:</strong>
+                    </p>
 
-                <div  className="reservation-item">
-                {event.reservations.map((reservation) => (
-                  <div key={reservation._id}>
-                    <p>
-                      <strong>Date:</strong>{" "}
-                      {new Date(reservation.date).toLocaleDateString()}
-                    </p>
-                    <p>
-                      <strong>Time:</strong> {reservation.startTime} -{" "}
-                      {reservation.endTime}
-                    </p>
-                    <p>
-                      <strong>Motive:</strong> {reservation.motive}
-                    </p>
-                    <p>
-                      <strong>Facility:</strong> {reservation.facility}
-                    </p>
-                    <p>
-                      <strong>Effective:</strong> {reservation.effective}
-                    </p>
-                    <p>
-                      <strong>State:</strong> {reservation.state}
-                    </p>
+                    <div className="event-reservations">
+                      {event.reservations.map((reservation) => (
+                        <div className="event-reservation-item" key={reservation._id}>
+                          <p>
+                            <strong>Date:</strong>{" "}
+                            {new Date(reservation.date).toLocaleDateString()}
+                          </p>
+                          <p>
+                            <strong>Time:</strong> {reservation.startTime} -{" "}
+                            {reservation.endTime}
+                          </p>
+                          <p>
+                            <strong>Motive:</strong> {reservation.motive}
+                          </p>
+                          <p>
+                            <strong>Facility:</strong> {reservation.facility}
+                          </p>
+                          <p>
+                            <strong>Effective:</strong> {reservation.effective}
+                          </p>
+                          <p>
+                            <strong>State:</strong> {reservation.state}
+                          </p>
+                        </div>
+                      ))}
                     </div>
-                ))}
+                  </div>
                 </div>
-              </div>
-            </div>
-          ))}
-        </div>
+              </Panel>
+            ))}
+          </div>
 
-        <div className="section reservations-section">
-          <h2>Reservations</h2>
-          {reservations.map((reservation) => (
-            <div key={reservation._id} className="reservation-item">
-              <p>
-                <strong>Date:</strong>{" "}
-                {new Date(reservation.date).toLocaleDateString()}
-              </p>
-              <p>
-                <strong>Time:</strong> {reservation.startTime} -{" "}
-                {reservation.endTime}
-              </p>
-              <p>
-                <strong>Motive:</strong> {reservation.motive}
-              </p>
-              <p>
-                <strong>Facility:</strong> {reservation.facility}
-              </p>
-              <p>
-                <strong>Effective:</strong> {reservation.effective}
-              </p>
-              <p>
-                <strong>State:</strong> {reservation.state}
-              </p>
-            </div>
-          ))}
+          <div className="section reservations-section">
+            <h2>Reservations</h2>
+            {reservations.map((reservation) => (
+              <div key={reservation._id} className="reservation-item">
+                <p>
+                  <strong>Date:</strong>{" "}
+                  {new Date(reservation.date).toLocaleDateString()}
+                </p>
+                <p>
+                  <strong>Time:</strong> {reservation.startTime} -{" "}
+                  {reservation.endTime}
+                </p>
+                <p>
+                  <strong>Motive:</strong> {reservation.motive}
+                </p>
+                <p>
+                  <strong>Facility:</strong> {reservation.facility}
+                </p>
+                <p>
+                  <strong>Effective:</strong> {reservation.effective}
+                </p>
+                <p>
+                  <strong>State:</strong> {reservation.state}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
